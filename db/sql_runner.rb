021c1f927e052818bfa3b773665872ba90a0154b
yossaj <yossaj@icloud.com>
2019-06-11 16:05:35 +0100

    added new server details

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,10 +3,10 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-    db = PG.connect({dbname: '',
-                    host: '',
-                    user:'',
-                    password:''})
+    db = PG.connect({dbname: 'ddsd70ak130oq8',
+                    host: 'ec2-174-129-242-183.compute-1.amazonaws.com',
+                    user:'qnruzumwsrpkrs',
+                    password:'4a13f8a697e3a2c43a758fef5e8a683ba915a5d4a4f76cbb17fbbe90d7be7295'})
     db.prepare("query", sql)
     result = db.exec_prepared('query',values)
     ensure
